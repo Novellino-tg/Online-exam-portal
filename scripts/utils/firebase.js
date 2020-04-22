09c680be04131876201f7bd76fc0a6c9234cb7ba
Tushar goyal <63923889+Novellino-tg@users.noreply.github.com>
2020-04-22 18:25:17 +0530

    Add files via upload

---
+++ scripts/utils/firebase.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA6TsNMUHLzuinxg7058Pm-V1W_HHaYDPc",
+    authDomain: "onlineexam-6220d.firebaseapp.com",
+    databaseURL: "https://onlineexam-6220d.firebaseio.com",
+    projectId: "onlineexam-6220d",
+    storageBucket: "",
+    messagingSenderId: "802800940584",
+    appId: "1:802800940584:web:394ed248f8ad9fb6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
